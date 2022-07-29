6ae9ebf2e2e39577b00e7996d916439e007ddefa
Jonathan Almeida <almeidajonathaan@gmail.com>
2022-07-29 07:43:13 -0300

    add reset em algumas informações ao abrir o modal

--- pizzaria/script.js
+++ pizzaria/script.js
@@ -1,4 +1,7 @@
+let modalQtd = 1;
+
 pizzaJson.map((item, index) => {
+    //clonando estrutura e preenchendo com as informações de cada pizza
     let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
     pizzaItem.querySelector('.pizza-item--img img').setAttribute('src', item.img);
     pizzaItem.querySelector('.pizza-item--price').innerHTML = ` R$ ${item.price.toFixed(2).replace('.',',')}`;
@@ -6,26 +9,40 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaItem.setAttribute('data-key', index);
     
-    
+    //adicionando evento de click para abrir o modal
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
+        //reset quantidade de pizza 
+        modalQtd = 1;
+        //removendo a seleção do tamanho escolhido anteriormente
+        document.querySelector('.pizzaInfo--size.selected').classList.remove('selected');
+        //adicionando a seleção ao tamanho grande
+        document.querySelectorAll('.pizzaInfo--size')[2].classList.add('selected');
         
+        //selecionando o atributo data-key para identificar a pizza
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
-        // console.log(key);
-        // console.log(pizzaJson[key]);
-        
-        document.querySelector('.pizzaBig img').src = pizzaJson[key].img;
-        document.querySelector('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
-        document.querySelector('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
-        document.querySelector('.pizzaInfo--actualPrice').innerHTML += pizzaJson[key].price.toFixed(2).replace('.',',');
-        
+
+        //criando uma animação ao abrir o modal
         document.querySelector('.pizzaWindowArea').style.opacity = 0; 
         document.querySelector('.pizzaWindowArea').style.display = 'flex';
         setTimeout(() => {
             document.querySelector('.pizzaWindowArea').style.opacity = 1; 
-        }, 200)
+        }, 200);
+        
+        //preenchendo o modal com as informações da pizza selecionada
+        document.querySelector('.pizzaBig img').src = pizzaJson[key].img;
+        document.querySelector('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        document.querySelector('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        document.querySelector('.pizzaInfo--actualPrice').innerHTML += pizzaJson[key].price.toFixed(2).replace('.',',');
+        document.querySelectorAll('.pizzaInfo--size').forEach((size, indexSize) => {
+            //adicionando os tamanhos das pizza de acordo com o json 
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[indexSize];
+        });
     });
+
+        document.querySelector('.pizzaInfo--qt').innerHTML = modalQtd;
     
+    //adicionando os campos preenchidos dentro da div
     document.querySelector('.pizza-area').append( pizzaItem );
 });
 
